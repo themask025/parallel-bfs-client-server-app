e73bbbd7fe8a55089d9f381134cecceab9180ca9
themask025 <evelintodo@gmail.com>
2024-01-03 13:31:48 +0200

    Refactored and cleaned code

--- client.py
+++ client.py
@@ -1,49 +1,48 @@
-# # echo client
-
-# # 2 - Client initiates a connection
-
-# import socket
-
-# HOST = "localhost"
-# PORT = 12345
-
-# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-#     s.connect((HOST, PORT))
-
-# # 3 - Data is exchanged
-
-#     s.sendall(b"Hello, world")
-#     data = s.recv(1024)
-
-# print(f"Received {data}")
 
 import socket
+import pickle
+
 
 HEADER = 16
-PORT = 6969
+PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDR)
+
+def send_graph(data):
+    client.send(pickle.dumps(data))
+
 
 def send(msg):
     message = msg.encode(FORMAT)
+
     msg_length = len(message)
     send_length = str(msg_length).encode(FORMAT)
     send_length += b' ' * (HEADER - len(send_length))
     client.send(send_length)
+
     client.send(message)
     print("Message sent!")
+
+
+def start():
+    print(f"Connecting to {ADDR} ...")
+    client.connect(ADDR)
     print(client.recv(2048).decode(FORMAT))
 
-send("Hello world!")
-input()
-send("Hello everyone!")
-input()
-send("Hello user!")
-input()
+    send("Hello world!")
+    print(client.recv(2048).decode(FORMAT))
 
-send(DISCONNECT_MESSAGE)
+    send(DISCONNECT_MESSAGE)
+
+
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+graph = {'A': ['B', 'C'], 'B': ['A', 'D', 'E'], 'C': ['A', 'F', 'G']}
+start_node = 'A'
+
+print("[STARTING] client is starting...")
+start()
--- server.py
+++ server.py
@@ -1,40 +1,22 @@
-# # echo server 
-
-# import socket
-
-# # 1 - Server sets up a listening socket
-
-# HOST = "localhost"
-# PORT = 12345
-
-# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-#     s.bind((HOST, PORT))
-#     s.listen()
-#     conn, addr = s.accept()
-
-#     with conn:
-#         print(f"Connected by {addr}")
-#         while True:
-#             data = conn.recv(1024)
-#             if not data:
-#                 break
-#             conn.sendall(data)
 
 import socket
 import threading
+import pickle
+
 
 HEADER = 16
-PORT = 6969
+PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(ADDR)
 
 def handle_client(conn, addr):
+
     print(f"[NEW CONNECTION] {addr} connected.")
+
+    conn.send("Connected!".encode(FORMAT))
 
     connected = True
     while connected:
@@ -42,6 +24,7 @@ def handle_client(conn, addr):
         if msg_length:
             msg_length = int(msg_length)
             msg = conn.recv(msg_length).decode(FORMAT)
+
             if msg == DISCONNECT_MESSAGE:
                 connected = False
             
@@ -60,6 +43,9 @@ def start():
         thread.start()
         print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
 
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
 
 print("[STARTING] server is starting...")
 start()
